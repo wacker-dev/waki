e51c815fac1de59bf8af6336612bfdb0d9214993
Xinzhao Xu <z2d@jifangcheng.com>
2024-11-29 13:13:53 +0800

    Initiate the HTTP connection before writing the body stream

--- test-programs/src/bin/client_post_with_body.rs
+++ test-programs/src/bin/client_post_with_body.rs
@@ -8,15 +8,20 @@ struct Data {
 }
 
 fn main() {
-    let buffer = [0; 5000];
+    // Make sure the final body is larger than 1024*1024, but we cannot allocate
+    // so much memory directly in the wasm program, so we use the `repeat`
+    // method to increase the body size.
+    const LEN: usize = 1024;
+    const REPEAT: usize = 1025;
+    let buffer = [0; LEN];
     let resp = Client::new()
         .post("https://httpbin.org/post")
-        .body(buffer)
+        .body(buffer.repeat(REPEAT))
         .connect_timeout(Duration::from_secs(5))
         .send()
         .unwrap();
     assert_eq!(resp.status_code(), 200);
 
     let data = resp.json::<Data>().unwrap();
-    assert_eq!(data.data.len(), 5000);
+    assert_eq!(data.data.len(), LEN * REPEAT);
 }
